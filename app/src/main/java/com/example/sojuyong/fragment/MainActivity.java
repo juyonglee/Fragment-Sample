4964e3edff705fd07f55079d37afced9c2c01d63
juyonglee <juyonglee0208@gmail.com>
2018-07-24 21:47:00 +0900

    LayoutInflater Example

--- app/src/main/java/com/example/sojuyong/fragment/MainActivity.java
+++ app/src/main/java/com/example/sojuyong/fragment/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.sojuyong.fragment;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void viewAdd01() {
+
+    }
+
+    public void viewAdd02() {
+         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+         View view = inflater.inflate(R.layout.activity_main, null);
+         setContentView(view);
+    }
+
+    public void viewAdd03() {
+         LayoutInflater inflater = getLayoutInflater();
+         View view = inflater.inflate(R.layout.activity_main, null);
+         setContentView(view);
+    }
+
+    public void viewAdd04() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.activity_main, null);
+        setContentView(view);
     }
 }
